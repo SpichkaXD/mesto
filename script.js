27bd426d30954dbac2f916ba98f4661dcd450569
Egor_lubchik <spichkaxd@yandex.by>
2022-05-05 21:54:55 +0300

    добавлена функция отключения кнопки

--- script.js
+++ script.js
@@ -183,6 +183,9 @@ const formInput = popupForm.querySelector(".popup__input");
 const formError = popupForm.querySelector(`.${formInput.id}-error`);
 const popupProfileAdd = popupProfile.querySelector(".popup-profile__btn-add");
 
+// стили кнопки
+popupProfileAdd.classList.add("popup__btn_value_save-error");
+
 const showInputError = (formElement, inputElement, errorMessage) => {
     // Выбираем элемент ошибки на основе уникального класса
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
@@ -191,17 +194,13 @@ const showInputError = (formElement, inputElement, errorMessage) => {
 
     errorElement.classList.add("popup__input-texterror_active");
     errorElement.textContent = errorMessage;
-    // стили кнопки
-    popupProfileAdd.classList.add("popup__btn_value_save-error");
 };
 
 const hideInputError = (formElement, inputElement) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.remove("popup__input-error");
     errorElement.classList.remove("popup__input-texterror_active");
-    errorElement.textContent = '';
-    // стили кнопки
-    popupProfileAdd.classList.remove("popup__btn_value_save-error");
+    errorElement.textContent = "";
 };
 
 //Функция, которая проверяет валидность поля
@@ -215,16 +214,44 @@ const checkInputValidity = (formElement, inputElement) => {
     }
 };
 
+// Функция принимает массив полей
+
+const hasInvalidInput = (inputList) => {
+    // проходим по этому массиву методом some
+    return inputList.some((inputElement) => {
+        // Если поле не валидно, колбэк вернёт true
+        // Обход массива прекратится и вся функция
+        // hasInvalidInput вернёт true
+        return !inputElement.validity.valid;
+    });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+    // Если есть хотя бы один невалидный инпут
+    if (hasInvalidInput(inputList)) {
+        // сделай кнопку неактивной
+        buttonElement.classList.add("popup__btn_value_save-error");
+    } else {
+        // иначе сделай кнопку активной
+        buttonElement.classList.remove("popup__btn_value_save-error");
+    }
+};
+
 const setEventListeners = (formElement) => {
     // Находим все поля внутри формы,
     const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+    // Найдём в текущей форме кнопку отправки
+    const buttonElement = formElement.querySelector(".popup__btn_value_save");
+    toggleButtonState(inputList, buttonElement);
     // Обойдём все элементы полученной коллекции
     inputList.forEach((inputElement) => {
         // каждому полю добавим обработчик события input
         inputElement.addEventListener("input", () => {
-            // Внутри колбэка вызовем isValid,
+            // Внутри колбэка вызовем checkInputValidity,
             // передав ей форму и проверяемый элемент
             checkInputValidity(formElement, inputElement);
+            // Вызовем toggleButtonState и передадим ей массив полей и кнопку
+            toggleButtonState(inputList, buttonElement);
         });
     });
 };
@@ -249,3 +276,5 @@ const enableValidation = () => {
 
 // Вызовем функцию
 enableValidation();
+
+
